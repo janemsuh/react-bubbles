ca9a150fe6ce598af3455a579d2df2f65cfc94ae
Jane Suh <sourcherryyogurt@gmail.com>
2020-01-24 22:12:50 -0500

    Modified routes using Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import BubblePage from './components/BubblePage';
@@ -10,13 +10,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Route exact path="/" component={Login} /> */}
-        <Route exact path='/' component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-        <PrivateRoute exact path='/bubbles' component={BubblePage} />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          {/* 
+            Build a PrivateRoute component that will 
+            display BubblePage when you're authenticated 
+          */}
+          <PrivateRoute exact path='/bubbles' component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
